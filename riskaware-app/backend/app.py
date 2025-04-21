277271e56b5ff0f5ea5cc22e12743e6aa5bfc4ea
aouyang3 <73046283+aouyang3@users.noreply.github.com>
2025-04-21 04:11:55 -0400

    Update app.py

--- riskaware-app/backend/app.py
+++ riskaware-app/backend/app.py
@@ -41,8 +41,15 @@ if not firebase_admin._apps:
     cred = credentials.Certificate({
         "type": "service_account",
         "project_id": os.environ.get("FIREBASE_PROJECT_ID"),
+        "private_key_id": os.environ.get("FIREBASE_PRIVATE_KEY_ID"),
         "private_key": os.environ.get("FIREBASE_PRIVATE_KEY").replace('\\n', '\n'),
         "client_email": os.environ.get("FIREBASE_CLIENT_EMAIL"),
+        "client_id": "100396240001888195546",
+        "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+        "token_uri": "https://oauth2.googleapis.com/token",
+        "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+        "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-fbsvc%40cs6440-ca243.iam.gserviceaccount.com",
+        "universe_domain": "googleapis.com",
     })
     initialize_app(cred)
 
